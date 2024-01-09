76a1d0e0ac14e48c5200c79107b865515ced64f6
ronyderra <derrarony@gmail.com>
2024-01-09 18:30:46 +0400

    port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import useragent from "express-useragent";
 import cors from "cors";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3030;
 
 app.use(useragent.express());
 app.use(cors());
